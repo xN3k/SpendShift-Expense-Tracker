49a73f4e24cc49bd649026507949f1b952584d50
xN3k <aadarshchaudhary463@gmail.com>
2023-12-25 23:32:44 +0545

    Added app Icon for android and iOS

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ void main() {
   runApp(
     MaterialApp(
       darkTheme: ThemeData.dark().copyWith(
-        useMaterial3: true,
         colorScheme: kDarkColorScheme,
         cardTheme: const CardTheme().copyWith(
           color: kDarkColorScheme.secondaryContainer,
@@ -31,7 +30,6 @@ void main() {
         ),
       ),
       theme: ThemeData().copyWith(
-        useMaterial3: true,
         colorScheme: kColorScheme,
         appBarTheme: const AppBarTheme().copyWith(
           backgroundColor: kColorScheme.onPrimaryContainer,
